7c3ec9166df2d6383520ea4c63d0a86f193228d9
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2013-10-28 15:43:49 +0100

    For public release

--- sinfa.R
+++ sinfa.R
@@ -13,26 +13,22 @@
 ##     You should have received a copy of the GNU General Public License
 ##     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+## version: 0.1 (this is not a git version)
+
+## To understand what this does, read Wang an Bilger, JASA vol 54(5), 1973, and type
+# source("sinfa.R")
+# read.wang.bilger()
+# x
+# f
+# s <- sinfa(x, f, 7)
+# s
+# summary(s)
+
 
 read.wang.bilger <- function() {
   assign("x", read.csv("wang-bilger-table6.csv", row.names=1), envir=.GlobalEnv)
   assign("f", read.csv("wang-bilger-features.csv", row.names=1), envir=.GlobalEnv)
   cons <- colnames(x)
-}
-
-read.natalie <- function(av="ao", noise="noise") {
-  global <- function(x, y) assign(x, y, env=.GlobalEnv)
-  f <- read.table("data/features.txt")
-  f <- data.frame(t(f))                 # transpose
-  global("conditions", c("bao", "bat", "con", "hel", "hoo", "niq", "rub", "sur", "tap"))
-  g <- function(x) {
-    x <- read.csv(sprintf("data/cm_%s_%s - %s.csv", av, noise, toupper(x)), row.names=1)
-    x[-nrow(x),-ncol(x)]           # ditch totals
-  }
-  x <- lapply(conditions, g)
-  names(x) <- conditions
-  global("x", x)
-  global("f", f)
 }
 
 ## cond can be multi-dimensional, i.e., an array of nphones x nfeatures
@@ -99,140 +95,91 @@ cond.mutual <- function(x, feature, cond=NULL) {
 
 
 # run a sinfa for confusion matrix x and feature data frame f
-run.sinfa <- function(x, f, nmax=1) {
+sinfa <- function(x, f, nmax=1) {
   stopifnot(all(row.names(x)==row.names(f)))
   f.names <- names(f)                   # current list of features to tabulate
   cond.f <- NULL
   cond.names <- NULL
-  cond.pasted.names <- "nothing"
+##  cond.pasted.names <- "nothing"
   g <- function(fea) c(ent=cond.entropy(x, f[[fea]], cond.f),
                        mut=cond.mutual(x, f[[fea]], cond.f))
   n <- 1
+  tot <- list()
   while(n<=nmax) {
     res <- data.frame(t(sapply(f.names, g)))
+    if (sum(res$ent)==0) {
+      warning("No more entropy left\n")
+      break
+    }
+    no.info <- res$ent==0
     res$rel <- res$mut / res$ent
-    cat(sprintf("Iteration %d: keeping %s constant\n", n, cond.pasted.names))
-    print(res)
-    max.fea <- which.max(res$rel)
+    res$rel[no.info] <- NA
+#    if (verbose) {
+#      cat(sprintf("Iteration %d: keeping %s constant\n", n, cond.names))
+#      print(res)
+#    }
+    max.fea <- which(res$rel == max(res$rel,na.rm=T))
+    tot[[n]] <- list(table=res[!no.info,], max=f.names[max.fea],
+                     no.info=f.names[no.info])
     cond.names <- append(cond.names, f.names[max.fea])
-    f.names <- f.names[-max.fea]
+    f.names <- f.names[-c(max.fea,no.info)]
     cond.f <- sapply(cond.names, function(x) f[[x]])
+##    cond.pasted.names <- paste(cond.names, collapse=":")
+##    if (verbose)
+##      cat(sprintf("Max at feature '%s'\n\n", cond.names[length(cond.names)]))
+    n <- n+1
+  }
+  class(tot) <- "sinfa"
+  tot
+}
+
+## default way of printing the SINFA analysis
+print.sinfa <- function(x) {
+  stopifnot(is(x, "sinfa"))
+  res <- NULL
+  cond.pasted.names <- "nothing"
+  cond.names <- NULL
+  n <- 1
+  for (iter in x) {
+    cat(sprintf("Iteration %d: knowing %s\n", n, cond.pasted.names))
+    print(iter$table)
+    if (length(iter$no.info)) {
+      cat(sprintf("No more information from '%s'\n", iter$no.info))
+      cond.names <- append(cond.names, paste(iter$no.info, "=0", sep=""))
+    }
+    maxinfo <- paste(iter$max, collapse="/")
+    cat(sprintf("Max relative information at feature '%s'\n\n", maxinfo))
+    cond.names <- append(cond.names, maxinfo)
     cond.pasted.names <- paste(cond.names, collapse=":")
-    cat(sprintf("Max at feature '%s'\n\n", cond.names[length(cond.names)]))
     n <- n+1
   }
 }
 
-## from here onwards old code
-##
-##
-
-
-entropy <- function(x, feature, margin=1) {
-  pm <- apply(x, margin, sum)/sum(x)
-  p <- tapply(pm, feature, sum)
-  lp <- log(p + (p==0), base=2)           # fix log
-  return(-sum(p*lp))
-}
-
-cond.entropy1 <- function(x, feature, cond, margin=1) {
-  pm <- apply(x, margin, sum)/sum(x)
-  index.cond <- tapply(numeric(length(cond)), cond)
-  nlevels.cond <- max(index.cond)
-  index.feature <- tapply(numeric(length(feature)), feature)
-  nlevels.feature <- max(index.feature)
-  p <- matrix(nrow=nlevels.cond, ncol=nlevels.feature)
-  for (i in 1:nlevels.cond) for (j in 1:nlevels.feature) {
-    p[i,j] <- sum(pm[index.cond==i & index.feature==j])
+## Summary of the analysis, just show the line of maximum information features.       
+summary.sinfa <- function(x) {
+  stopifnot(is(x, "sinfa"))
+  res <- NULL
+  for (iter in x) {
+    m <- iter$max[1]
+    line <- iter$table[m,]
+    row.names(line) <- paste(iter$max, collapse="/")
+    res <- rbind(res, line)
   }
-  pi <- tapply(pm, cond, sum)
-  arg <- p / matrix(pi, nrow=nlevels.cond, ncol=nlevels.feature, byrow=F) 
-  lp <- log(arg + (arg==0), base=2)
-  return(-sum(p*lp))
+  transform(res, cum.ent=cumsum(ent), cum.mut=cumsum(mut))
 }
 
-
-mutual <- function(x, feature) {
-  index <- tapply(numeric(length(feature)), feature)
-  nlevels <- max(index)
-  p <- matrix(nrow=nlevels, ncol=nlevels)
-  n <- sum(x)
-  for (i in 1:nlevels) for (j in 1:nlevels) {
-    p[i,j] <- sum(x[index==i,index==j])/n
+read.natalie <- function(av="ao", noise="noise") {
+  global <- function(x, y) assign(x, y, env=.GlobalEnv)
+  f <- read.table("data/features.txt")
+  f <- data.frame(t(f[row.names(f)!="vcless",]))                 # transpose, remove vcless
+  global("conditions", c("bao", "bat", "con", "hel", "hoo", "niq", "rub", "sur", "tap"))
+  g <- function(x) {
+    x <- read.csv(sprintf("data/cm_%s_%s - %s.csv", av, noise, toupper(x)), row.names=1)
+    x[-nrow(x),-ncol(x)]           # ditch totals
   }
-  pi <- apply(p, 1, sum)
-  pj <- apply(p, 2, sum)
-  arg <- outer(pi, pj, '*')/p
-  lp <- log(arg, base=2)
-  lp[is.nan(lp) | lp==Inf] <- 0         # fix p->0 cases
-  return(-sum(p*lp))
+  x <- lapply(conditions, g)
+  names(x) <- conditions
+  global("x", x)
+  global("f", f)
 }
 
-rel.transmission <- function(x, feature, margin=1) 
-  return(mutual(x, feature) / entropy(x, feature, margin))
-
-cond.mutual1 <- function(x, feature, cond) {
-  index.cond <- tapply(numeric(length(cond)), cond)
-  nlevels.cond <- max(index.cond)
-  index.feature <- tapply(numeric(length(feature)), feature)
-  nlevels.feature <- max(index.feature)
-  p <- array(0, dim=c(nlevels.cond,nlevels.feature,nlevels.feature))
-  n <- sum(x)
-  for (i in 1:nlevels.cond) for (j in 1:nlevels.feature) for (k in 1:nlevels.feature) {
-    y <- x[index.cond==i & index.feature==j, index.cond==i & index.feature==k]
-    if (prod(dim(y))>0) 
-      p[i,j,k] <- sum(y)/n
-  }
-  pi <- apply(p, 1, sum)
-  pij <- apply(p, c(1,2), sum)
-  pik <- apply(p, c(1,3), sum)
-  arg <- sweep(p, 1, pi, '*')
-  arg <- sweep(arg, c(1:2), pij, '/')
-  arg <- sweep(arg, c(1,3), pik, '/')
-  lp <- log(arg, base=2)
-  lp[is.nan(lp) | lp==-Inf] <- 0                   # lot of 0/0's out there
-  return (sum(p * lp))
-}
-  
-# alternative computation, same result. 
-cond.entropy2 <- function(x, feature, cond, margin=1) {
-  pm <- apply(x, margin, sum)/sum(x)
-  index.cond <- tapply(numeric(length(cond)), cond)
-  nlevels.cond <- max(index.cond)
-  index.feature <- tapply(numeric(length(feature)), feature)
-  nlevels.feature <- max(index.feature)
-  p <- matrix(nrow=nlevels.cond, ncol=nlevels.feature)
-  for (i in 1:nlevels.cond) for (j in 1:nlevels.feature) {
-    p[i,j] <- sum(pm[index.cond==i & index.feature==j]) / sum(pm[index.cond==i])
-  }
-  pi <- tapply(pm, cond, sum)
-  lp <- log(p + (p==0), base=2)
-  ent <- apply(p*lp, 1, sum)
-  return(-sum(pi*ent))
-}
-
-# this function computes outer products varying only the first dimension,
-# while keeping the remaining dimensions constant
-partial.outer <- function(x, y, FUN) {
-  nd <- length(dim(x))
-  d.const <- dim(x)[2:nd]
-  stopifnot(nd == length(dim(y)), all(d.const==dim(y)[2:nd]))
-  nd.const <- prod(d.const)
-  x <- array(x, dim=c(dim(x)[1], nd.const))
-  y <- array(y, dim=c(dim(y)[1], nd.const))
-  res <- array(NA, dim=c(dim(x)[1], dim(y)[1], nd.const))
-#  print(sprintf("Output dim %d, %d, %d", dim(x)[1], dim(y)[1], d.const))
-  for (i in 1:nd.const)
-    res[,,i] <- outer(x[,i], y[,i], FUN)
-  dim(res) <- c(dim(x)[1], dim(y)[1], d.const)
-  res                
-}
-
-## features
-#voice <- cons %in% c("b", "d", "g", "v", "dh", "z", "zj", "dzj")
-#fric <- cons %in% c("f", "th", "s", "sh", "v", "dh", "z", "zj", "tsh", "dzj")
-#sib <- cons %in% c("s", "sh", "z", "zj", "tsh", "dzj")
-#str <- cons %in% c("f", "s", "sh", "v", "z", "zj", "tsh", "dzj")
-#dur <- cons %in% c("s", "sh", "z", "zj")
-#pl1 <- c(0, 1, 2, 0, 1, 2, 0, 1, 1, 2, 0, 1, 1, 2, 2, 2)
-#high <- cons %in% c("k", "g", "sh", "zj", "tsh", "dzj")
